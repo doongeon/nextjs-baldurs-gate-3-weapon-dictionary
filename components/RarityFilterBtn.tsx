64c3e00ac81ef0f0c67c70b8a32c56ebda4513a7
donggeon Na <ndmb2012@gmail.com>
2024-10-18 15:12:19 +0900

    fix: styled components prop

--- components/RarityFilterBtn.tsx
+++ components/RarityFilterBtn.tsx
@@ -5,81 +5,83 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 
 interface RarityFilterBtnProps {
-  text: string;
-  rarity: Rarity;
+    text: string;
+    rarity: Rarity;
 }
 
-const Button = styled.button<{ rarity: Rarity; active: boolean }>`
-  color: ${(props) =>
-    props.active ? "rgba(255,255,255, 0.8)" : getRarityColor(props.rarity)};
-  font-weight: 400;
-  border: 1px solid ${(props) => getRarityColor(props.rarity)};
-  text-shadow: ${(props) => "0px 0px 5px " + getRarityColor(props.rarity)};
-  box-shadow: ${(props) =>
-    "0px 0px 15px " +
-    getRarityColor(props.rarity) +
-    ", inset 0px 0px 5px " +
-    getRarityColor(props.rarity)};
-  transition: linear 0.4s;
-  position: relative;
-
-  &:after {
-    content: "";
-    z-index: -1;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    filter: blur(10px);
-    background-color: ${(props) =>
-      props.active ? getRarityColor(props.rarity) : "transparent"};
+const ShiningBtn = styled.button<{ $rarity: Rarity; $active: boolean }>`
+    color: ${(props) =>
+        props.$active
+            ? "rgba(255,255,255, 0.8)"
+            : getRarityColor(props.$rarity)};
+    font-weight: 400;
+    border: 1px solid ${(props) => getRarityColor(props.$rarity)};
+    text-shadow: ${(props) => "0px 0px 5px " + getRarityColor(props.$rarity)};
+    box-shadow: ${(props) =>
+        "0px 0px 15px " +
+        getRarityColor(props.$rarity) +
+        ", inset 0px 0px 5px " +
+        getRarityColor(props.$rarity)};
     transition: linear 0.4s;
-  }
+    position: relative;
+
+    &::after {
+        content: "";
+        z-index: -1;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        filter: blur(10px);
+        background-color: ${(props) =>
+            props.$active ? getRarityColor(props.$rarity) : "transparent"};
+        transition: linear 0.4s;
+    }
 `;
 
 export default function RarityFilterBtn({
-  text,
-  rarity,
+    text,
+    rarity,
 }: RarityFilterBtnProps) {
-  const [rarityFilter, setRarityFilter] = useRecoilState(rarityFilterState);
-  const [active, setActive] = useState(false);
+    const [rarityFilter, setRarityFilter] = useRecoilState(rarityFilterState);
+    const [active, setActive] = useState(false);
 
-  const toggleSet = (filter: Rarity) => {
-    setRarityFilter((set) => {
-      const newSet = new Set(set);
+    const toggleSet = (filter: Rarity) => {
+        setRarityFilter((set) => {
+            const newSet = new Set(set);
 
-      if (!newSet.has(filter)) {
-        newSet.add(filter);
-        return newSet;
-      }
+            if (!newSet.has(filter)) {
+                newSet.add(filter);
+                return newSet;
+            }
 
-      newSet.delete(filter);
-      return newSet;
-    });
-  };
+            newSet.delete(filter);
+            return newSet;
+        });
+    };
 
-  const handleClick = (filter: Rarity) => {
-    toggleSet(filter);
-  };
+    const handleClick = (filter: Rarity) => {
+        toggleSet(filter);
+    };
 
-  useEffect(() => {
-    if (rarityFilter.has(rarity)) {
-      setActive(true);
-      return;
-    }
+    useEffect(() => {
+        if (rarityFilter.has(rarity)) {
+            setActive(true);
+            return;
+        }
 
-    setActive(false);
-  }, [rarityFilter]);
+        setActive(false);
+    }, [rarityFilter]);
 
-  return (
-    <Button
-      rarity={rarity}
-      active={active}
-      onClick={() => handleClick(rarity)}
-      className="cursor-pointer px-1 py-1 sm:px-2 sm:text-base select-none text-xs"
-    >
-      {text}
-    </Button>
-  );
+    return (
+        <ShiningBtn
+            $rarity={rarity}
+            $active={active}
+            onClick={() => handleClick(rarity)}
+            className="cursor-pointer px-1 py-1 sm:px-2 sm:text-base select-none text-xs"
+        >
+            {text}
+        </ShiningBtn>
+    );
 }
