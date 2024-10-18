b983f58f836ac7f2d719996f80dd4151b2b8d243
donggeon Na <ndmb2012@gmail.com>
2024-10-18 15:13:08 +0900

    update: main image

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,15 +3,12 @@
 import Link from "next/link";
 
 export default function Title() {
-  return (
-    <div className="w-full max-w-screen-sm text-center">
-      <Link className="flex flex-col items-center gap-2" href={"/"}>
-        <img className="w-52" src="/images/favicon.ico" alt="favicon" />
-        <h1 className="text-6xl font-bold">발게삼</h1>
-      </Link>
-      <h2 className="text-1xl bg-red-600 rounded-xl w-20 text-center place-content-center my-2 ml-3">
-        BETA
-      </h2>
-    </div>
-  );
+    return (
+        <div className="w-full max-w-screen-sm text-center">
+            <Link className="flex flex-col items-center gap-2" href={"/"}>
+                <img className="w-52" src="/images/favicon.ico" alt="favicon" />
+                <h1 className="text-6xl font-bold">발게삼</h1>
+            </Link>
+        </div>
+    );
 }
